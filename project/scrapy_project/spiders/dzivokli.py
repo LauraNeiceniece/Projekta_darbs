49657632eb23fa9dd0c61adf233af4f32e470e32
LauraNeiceniece <144619907+LauraNeiceniece@users.noreply.github.com>
2024-01-16 20:24:52 +0000

    16.01.24

--- project/scrapy_project/spiders/dzivokli.py
+++ project/scrapy_project/spiders/dzivokli.py
@@ -1,4 +1,6 @@
 import csv
+import sys
+import re
 import scrapy
 from scrapy import signals
 from scrapy.crawler import CrawlerProcess
@@ -11,22 +13,38 @@ class DzivokliSpider(scrapy.Spider):
     def start_requests(self):
         URL = 'https://www.ss.lv/lv/real-estate/flats/riga/all/hand_over/'
         yield scrapy.Request(url=URL, callback=self.response_parser)
- 
+
     def response_parser(self, response):
+
+        region = sys.argv[1]
+        maxRent = int(sys.argv[2])
+        minRoomCount = int(sys.argv[3])
+        orderCol = sys.argv[4]
         for selector in response.css('tr[id^="tr_"]'):
             relative_link = selector.css('a::attr(href)').extract_first()
             absolute_link = urljoin(response.url, relative_link)
+            info=selector.css('td.msga2-o.pp6::text, td.msga2-o.pp6 b::text').getall()
+            rent_r=info[6]
+            match = re.search(r'\b\d+\b', rent_r)
+            if match:
+                rent = int(match.group())
+            else:
+                print("No integer value found.")
+            if info[0]==region and rent<=maxRent and int(info[2])>=minRoomCount:
+                if len(info)<5:
+                    continue
 
-            yield {
-                  
-                'address': selector.css('td.msga2-o.pp6::text, td.msga2-o.pp6 b::text').getall(),
-                'hipersaite': absolute_link
-                #'rooms': selector.css('td.msga2-o.pp6::text').getall(),
-                #'hipersaite': selector.css('a::attr(href)').extract_first()
-            }
-        #next_page = response.css('a.navi[rel="next"]::attr(href)').extract_first()
-        #if next_page:
-           #yield response.follow(next_page, callback=self.response_parser)
+                yield {
+       
+                        'adrese': info[1],
+                        'istabu skaits':info[2],
+                        'platība m2':info[3],
+                        'cena':info[6],
+                        'hipersaite': absolute_link
+                    }
+            next_page = response.css('a.navi[rel="next"]::attr(href)').extract_first()
+            if next_page:
+                yield response.follow(next_page, callback=self.response_parser)
 
 def dzivokli_spider_result():
     dzivokli_results = []
@@ -41,10 +59,16 @@ def dzivokli_spider_result():
     return dzivokli_results
 
 if __name__ == '__main__':
-    dzivokli_data=dzivokli_spider_result()
+    if len(sys.argv) != 5:
+        print("Usage: python project/scrapy_project/spiders/dzivokli.py <region> <max-rent> <min-room-count> <order-col>")
+    else:
 
-    keys = dzivokli_data[0].keys()
-    with open('dzivokli_data.csv', 'w', newline='') as output_file_name:
-        writer = csv.DictWriter(output_file_name, keys)
-        writer.writeheader()
-        writer.writerows(dzivokli_data)
+        dzivokli_data=dzivokli_spider_result()
+    if len(dzivokli_data)==0:
+        print("nav ierakstu")
+    else:
+        keys = dzivokli_data[0].keys()
+        with open('dzivokli_data.csv', 'w', newline='') as output_file_name:
+            writer = csv.DictWriter(output_file_name, keys)
+            writer.writeheader()
+            writer.writerows(dzivokli_data)
