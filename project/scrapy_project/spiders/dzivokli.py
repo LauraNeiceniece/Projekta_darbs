95a35c054e6e50b01d6aa50d7a38c8e2ea462183
LauraNeiceniece <144619907+LauraNeiceniece@users.noreply.github.com>
2024-01-16 15:34:57 +0000

    16.01.24

--- project/scrapy_project/spiders/dzivokli.py
+++ project/scrapy_project/spiders/dzivokli.py
@@ -3,6 +3,7 @@ import scrapy
 from scrapy import signals
 from scrapy.crawler import CrawlerProcess
 from scrapy.signalmanager import dispatcher
+from urllib.parse import urljoin
 
 class DzivokliSpider(scrapy.Spider):
     name = "dzivokli"
@@ -11,19 +12,21 @@ class DzivokliSpider(scrapy.Spider):
         URL = 'https://www.ss.lv/lv/real-estate/flats/riga/all/hand_over/'
         yield scrapy.Request(url=URL, callback=self.response_parser)
  
-
     def response_parser(self, response):
         for selector in response.css('tr[id^="tr_"]'):
-               
+            relative_link = selector.css('a::attr(href)').extract_first()
+            absolute_link = urljoin(response.url, relative_link)
+
             yield {
                   
-                'address': selector.css('td.msga2-o.pp6::text').getall(),
+                'address': selector.css('td.msga2-o.pp6::text, td.msga2-o.pp6 b::text').getall(),
+                'hipersaite': absolute_link
                 #'rooms': selector.css('td.msga2-o.pp6::text').getall(),
-                #'m2': selector.css('td.msg2 b a ::attr(href)').extract_first()
+                #'hipersaite': selector.css('a::attr(href)').extract_first()
             }
         #next_page = response.css('a.navi[rel="next"]::attr(href)').extract_first()
         #if next_page:
-         #   yield response.follow(next_page, callback=self.response_parser)
+           #yield response.follow(next_page, callback=self.response_parser)
 
 def dzivokli_spider_result():
     dzivokli_results = []
--- result.py
+++
@@ -1,5 +0,0 @@
-import requests
-import bs4
-import sys
-import scrapy
-
